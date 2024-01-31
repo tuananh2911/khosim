844ca0ff8a026d16a292c98922c6d3e28c3f9c9c
tuananhsonat <anhlt@sonat.vn>
2024-01-31 09:45:25 +0700

    init app

--- src/app/lienhe/introduction/index.tsx
+++ src/app/lienhe/introduction/index.tsx
@@ -5,7 +5,7 @@ const Introduction = () => {
         <div className="max-w-4xl my-4 mx-auto ml-4 p-6 bg-white shadow-md m-4">
             <h2 className="text-2xl font-bold border-b-2 border-gray-300 pb-2 mb-4">GIỚI THIỆU</h2>
             <p className="text-gray-700">
-                Hơn 15 năm - một chặng đường với sự hình thành và phát triển không ngừng nghỉ cho đến nay. Kho Sim là
+                Hơn 15 năm - một chặng đường với sự hình thành và phát triển không ngừng nghỉ cho đến nay. Sim Vip là
                 thương hiệu chuyên cung cấp Sim số đẹp lớn nhất và rẻ nhất thị trường, chiếm được niềm tin của hàng
                 triệu khách hàng trên toàn quốc.
                 {/* ... Nội dung tiếp theo ... */}
@@ -13,7 +13,7 @@ const Introduction = () => {
 
             <h2 className="text-2xl font-bold border-b-2 border-gray-300 pb-2 mt-4 mb-4">Về chúng tôi</h2>
             <p className="text-gray-700">
-                Khosim.com là website được công ty chúng tôi cho ra đời từ năm 2009 với mục đích phục vụ những người yêu
+                Simvipgiare.com là website được công ty chúng tôi cho ra đời từ năm 2009 với mục đích phục vụ những người yêu
                 thích sim số đẹp trên toàn quốc với đa dạng loại sim và phân loại mức giá phù hợp với mọi tầng lớp và
                 khả năng kinh tế của khách hàng.
                 {/* ... Nội dung tiếp theo ... */}
@@ -29,8 +29,8 @@ const Introduction = () => {
             <h2 className="text-2xl font-bold border-b-2 border-gray-300 pb-2 mt-4 mb-4">Thông tin liên hệ và hỗ trợ</h2>
             <p className="text-gray-700">
                 Để được tư vấn và hỗ trợ về việc mua/bán sim hay bất cứ vấn đề về sim số nào quý khách gặp phải, hãy
-                liên lạc với Kho sim qua thông tin sau:
-                {/* ... Nội dung tiếp theo ... */}
+                liên lạc với Sim Vip qua thông tin sau:
+                Zalo: 0886.39.4567
             </p>
         </div>
     );
--- src/app/payment/introduction/index.tsx
+++ src/app/payment/introduction/index.tsx
@@ -7,9 +7,9 @@ const Introduction = () => {
 
             <p className="text-gray-700 mb-4 font-bold">1. THANH TOÁN TIỀN MẶT</p>
 
-            <p className="text-gray-700 mb-4 ml-4">1.1 Tại siêu thị: Quý khách có thể đến mua hàng & thanh toán trực tiếp tại cửa hàng của khosim.com</p>
+            <p className="text-gray-700 mb-4 ml-4">1.1 Tại siêu thị: Quý khách có thể đến mua hàng & thanh toán trực tiếp tại cửa hàng của simvipgiare.com</p>
 
-            <p className="text-gray-700 mb-4 ml-4">1.2 Tại nhà/nơi nhận hàng (sim): Thanh toán cho nhân viên giao hàng trực tiếp của khosim.com tại bất kỳ đâu mà bạn yêu cầu khi giao hàng (sim).</p>
+            <p className="text-gray-700 mb-4 ml-4">1.2 Tại nhà/nơi nhận hàng (sim): Thanh toán cho nhân viên giao hàng trực tiếp của simvipgiare.com tại bất kỳ đâu mà bạn yêu cầu khi giao hàng (sim).</p>
 
             <p className="text-gray-700 mb-4 font-bold">2. THANH TOÁN CHUYỂN KHOẢN</p>
 
@@ -17,14 +17,14 @@ const Introduction = () => {
 
             <ul className="list-disc ml-8">
                 <li>Tên ngân hàng: Ngân hàng Thương mại Cổ phần Ngoại thương Việt Nam - Vietcombank</li>
-                <li>Chủ tài khoản: NGUYEN TRUNG NGUYEN</li>
-                <li>Số tài khoản: 9966666666</li>
-                <li>Nội dung: DH Mã đơn hàng - Số sim đặt mua (VD: DH686868 0911000888)</li>
+                <li>Chủ tài khoản: BUI TUAN KIET</li>
+                <li>Số tài khoản: 24.9999.6666</li>
+                <li>Nội dung: TÊN MUA HÀNG - Số sim đặt mua (VD: BUITUANKIET 0886394567)</li>
             </ul>
             <br/>
             <p className="text-gray-700 mb-4 font-bold">3. QUY ĐỊNH VỀ HOÀN TRẢ TIỀN</p>
 
-            <p className="text-gray-700 mb-4 ml-4">Trong trường hợp quý khách hàng đã mua hàng & thanh toán thành công nhưng dư tiền, hoặc đổi trả sim (chưa đăng ký thông tin), Kho Sim sẽ hoàn tiền qua hình thức chuyển khoản vào số tài khoản yêu cầu, hoặc tiền mặt tại địa chỉ giao hàng (sim).</p>
+            <p className="text-gray-700 mb-4 ml-4">Trong trường hợp quý khách hàng đã mua hàng & thanh toán thành công nhưng dư tiền, hoặc đổi trả sim (chưa đăng ký thông tin), Sim Vip sẽ hoàn tiền qua hình thức chuyển khoản vào số tài khoản yêu cầu, hoặc tiền mặt tại địa chỉ giao hàng (sim).</p>
         </div>
     );
 };
